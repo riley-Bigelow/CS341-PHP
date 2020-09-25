73657fbb9fdae523f4c52cce426edb460b620097
Riley <bigelow.riley.j@gmail.com>
2020-09-25 15:42:14 -0600

    adding comments

--- web/Teach/welcome_get.php
+++ web/Teach/welcome_get.php
@@ -3,7 +3,8 @@
 
 Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"];?><br> 
-You major is: <?php echo $_GET["major"];?>
+You major is: <?php echo $_GET["major"];?><br>
+Comments: <?php echo $_GET["comments"];?> <br>
 
 </body>
 </html>
