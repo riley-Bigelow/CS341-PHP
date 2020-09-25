8b9b522deb50230d59bf27b8040a8378312793fb
Riley <bigelow.riley.j@gmail.com>
2020-09-25 15:40:16 -0600

    adding major function

--- web/Teach/welcome_get.php
+++ web/Teach/welcome_get.php
@@ -2,7 +2,8 @@
 <body>
 
 Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
+Your email address is: <?php echo $_GET["email"];?><br> 
+You major is: <?php echo $_GET["major"];?>
 
 </body>
 </html>
