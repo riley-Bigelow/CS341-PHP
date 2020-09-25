95b8a6e28cf4acee4f58fe5d9d3b032b3b3aec55
Riley <bigelow.riley.j@gmail.com>
2020-09-25 15:49:56 -0600

    continents logic added

--- web/Teach/welcome_get.php
+++ web/Teach/welcome_get.php
@@ -5,6 +5,18 @@ Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"];?><br> 
 You major is: <?php echo $_GET["major"];?><br>
 Comments: <?php echo $_GET["comments"];?> <br>
+<?php
+
+if(empty($_POST['day'])){
+    echo "You live in the Ocean";
+}else{
+    $N = count($_POST['day']);
+    echo("You have visited $N Continent(s): ");
+    for($i=0; $i < $N; $i++){
+        echo($_POST['continents'][$i] . " ");
+    }
+}
+?>
 
 </body>
 </html>
