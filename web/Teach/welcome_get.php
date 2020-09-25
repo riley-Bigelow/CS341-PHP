db8929006bb5c73c802cf74491ce520eb4e35fee
Riley <bigelow.riley.j@gmail.com>
2020-09-25 16:05:56 -0600

    updates

--- web/Teach/welcome_get.php
+++ web/Teach/welcome_get.php
@@ -1,15 +1,16 @@
 <html>
 <body>
 
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"];?><br> 
-You major is: <?php echo $_GET["major"];?><br>
-Comments: <?php echo $_GET["comments"];?> <br>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"];?><br> 
+You major is: <?php echo $_POST["major"];?><br>
+Comments: <?php echo $_POST["comments"];?> <br>
 <?php
-if(!empty($_GET['continents'])){
+if(!empty($_POST['continents'])){
     // Loop to store and display values of individual checked checkbox.
-    foreach($_GET['continents'] as $selected){
+    foreach($_POST['continents'] as $selected){
     echo $selected."</br>";
+    }
 }
 ?>
 
