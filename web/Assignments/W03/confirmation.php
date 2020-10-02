2d504b8b558fec5f774bd1ec186dcb9aabeb39c5
Riley <bigelow.riley.j@gmail.com>
2020-10-02 10:01:23 -0600

    php

--- web/Assignments/W03/confirmation.php
+++ web/Assignments/W03/confirmation.php
@@ -42,6 +42,9 @@
         <div class="shop">
             <h1 id="cartMsg">Confirmation</h1>
         </div>
+        <php? 
+            var_dump($_POST);
+        ?>
         </div>
             <div class="container-fluid" id="footer">
         </div>
