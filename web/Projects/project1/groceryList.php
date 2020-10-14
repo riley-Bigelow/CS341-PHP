03ae7e34e5d897fe7f36ab4d9870102114a3ab0d
Riley <bigelow.riley.j@gmail.com>
2020-10-14 11:53:33 -0600

    hnages

--- web/Projects/project1/groceryList.php
+++ web/Projects/project1/groceryList.php
@@ -39,7 +39,7 @@
 					<div class="topnav">
 						<a href="cookBook.php"  id="cookbook"  class="button"><img src="cookbook.png" style="width:42px;height:42px;"><br>CookBook</a>
 						<a href="mealPlan.php" id ="mealplan"  class="button"><img src="mealplan.png" style="width:42px;height:42px;"><br>Meal Planner</a>
-						<a href="groceryList.php" id="glist"     class="button"><img src="grocerylist.png" style="width:42px;height:42px;"><br>Grocery List</a>		
+						<a href="groceryList.php" id="glist"     class="button"  style = "background-color: #ffff66"><img src="grocerylist.png" style="width:42px;height:42px;"><br>Grocery List</a>		
 					</div>
 					<div class="display">
 				
