ac110163c0c44bb7055223c1d29ab11ddd01cf5f
Riley <bigelow.riley.j@gmail.com>
2020-10-14 17:02:25 -0600

    potenital fix

--- web/Projects/project1/details.php
+++ web/Projects/project1/details.php
@@ -58,7 +58,7 @@
 									<h3>Instructions</h3>
 									<?php
 										$id = $_GET['id'];
-										$statement = $db->query('SELECT instructions FROM instructions Where deletedat IS NULL AND recipeid = :id');
+										$statement = $db->prepare('SELECT instructions FROM instructions Where deletedat IS NULL AND recipeid = :id');
 										$statement->execute(array(':id' => $id));
 										while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 										{
