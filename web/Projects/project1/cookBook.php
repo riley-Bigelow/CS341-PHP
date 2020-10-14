b0cb15ce969a853219c1846de41b9f15123d8321
Riley <bigelow.riley.j@gmail.com>
2020-10-14 11:47:06 -0600

    update to link menu

--- web/Projects/project1/cookBook.php
+++ web/Projects/project1/cookBook.php
@@ -37,7 +37,7 @@
 						</div>
 					</div>
 					<div class="topnav">
-						<a href="#home"  id="cookbook"  class="button"><img src="cookbook.png" style="width:42px;height:42px;"><br>CookBook</a>
+						<a href="#home"  id="cookbook"  class="button"><img src="cookbook.png" style="width:42px;height:42px;background-color: #ffff66;"><br>CookBook</a>
 						<a href="mealPlan.php" id ="mealplan"  class="button"><img src="mealplan.png" style="width:42px;height:42px;"><br>Meal Planner</a>
 						<a href="groceryList.php" id="glist"     class="button"><img src="grocerylist.png" style="width:42px;height:42px;"><br>Grocery List</a>
 						<a href="addRecipe.php" class="button" id='add'><img src="addrecipe.png" style="width:42px;height:42px;"><br>Add Recipe</a>
