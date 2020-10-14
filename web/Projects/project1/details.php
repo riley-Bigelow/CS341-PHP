bd5217e90d11e0fcaefc737842c0007cf6aa4af9
Riley <bigelow.riley.j@gmail.com>
2020-10-14 16:41:35 -0600

    test

--- web/Projects/project1/details.php
+++ web/Projects/project1/details.php
@@ -58,14 +58,13 @@
 									<h3>Instructions</h3>
 									<ul>
 									<?php
-										$statement = $db->query('SELECT instructions 
-																 FROM instructions 
-																Where deletedat IS NULL
-																AND recipeid = :id');
+										$statement = $db->query('SELECT instructions  FROM instructions Where deletedat IS NULL AND recipeid = :id');
+										$statement->execute(array(':id' => $id));
 										while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 										{
+											$instructions = $sRow["instructions"];
 											echo '<li>' .
-													'<p>'. $row['instructions'] . '</p>'.
+													'<p>'. $instructions . '</p>'.
 												 '</li>';
 										}
 									?>
