6bf69caa067202cacaad86402919eb024264e876
Riley <bigelow.riley.j@gmail.com>
2020-10-14 14:16:21 -0600

    query update

--- web/Projects/project1/cookBook.php
+++ web/Projects/project1/cookBook.php
@@ -51,7 +51,7 @@
 					<div class="display">
 						<ul id="recipeList" class = "results">
                             <?php
-                                $statement = $db->query('SELECT recipename FROM recipes');
+                                $statement = $db->query('SELECT recipename, recipeid FROM recipes');
                                 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
                                 {
 									echo '<li class="item"'. 'id="'. $row[recipeid].'">' .
