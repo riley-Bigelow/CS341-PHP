29ecce3735d1a3e49c8ea5a1519df1ed071e552a
Riley <bigelow.riley.j@gmail.com>
2020-10-14 11:19:28 -0600

    link updates

--- web/Projects/project1/mealPlan.php
+++ web/Projects/project1/mealPlan.php
@@ -37,7 +37,7 @@
 						</div>
 					</div>
 					<div class="topnav">
-						<a href="#home"  id="cookbook"  class="button"><img src="cookbook.png" style="width:42px;height:42px;"><br>CookBook</a>
+						<a href="cookBook.php"  id="cookbook"  class="button"><img src="cookbook.png" style="width:42px;height:42px;"><br>CookBook</a>
 						<a href="mealPlan.php" id ="mealplan"  class="button"><img src="mealplan.png" style="width:42px;height:42px;"><br>Meal Planner</a>
 						<a href="groceryList.php" id="glist"   class="button"><img src="grocerylist.png" style="width:42px;height:42px;"><br>Grocery List</a>
 					</div>
