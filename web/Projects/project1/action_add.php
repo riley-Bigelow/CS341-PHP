ada0bc91b9af174af6eaba3720cec303b1d50ea5
Riley <bigelow.riley.j@gmail.com>
2020-10-23 14:35:05 -0600

    testing

--- web/Projects/project1/action_add.php
+++ web/Projects/project1/action_add.php
@@ -29,11 +29,11 @@ if(isset($_POST['plan'])){
 	$planned = "True";
 };
 				
-for ($x = 0; $x <= count($_POST['ingred']); $x++) {
-	echo "The number is: $_POST['ingred'][$x]. <br>";
-	echo "The number is: $_POST['quant'][$x]. <br>";
-	echo "The number is: $_POST['meas'][$x]. <br>";
-  }
+for ($x = 0; $x <= count($ingredients); $x++) {
+	echo "The number is: $ingredients['ingred'][$x]. <br>";
+	echo "The number is: $quant['quant'][$x]. <br>";
+	echo "The number is: $measure['meas'][$x]. <br>";
+  };
 
 
 
