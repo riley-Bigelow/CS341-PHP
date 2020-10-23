00d1dde1c88bfa96d5cf836c86f35b7c3d06a4cc
Riley <bigelow.riley.j@gmail.com>
2020-10-23 08:19:55 -0600

    updates

--- web/Projects/project1/addRecipe.php
+++ web/Projects/project1/addRecipe.php
@@ -42,50 +42,50 @@
 						<a href="groceryList.php" id="glist"     class="button"><img src="grocerylist.png" style="width:42px;height:42px;"><br>Grocery List</a>		
 						<a href="addRecipe.php" class="button" id='add' style = "background-color: #ffff66"><img src="addrecipe.png" style="width:42px;height:42px;"><br>Add Recipe</a>
 					</div>
-						<div class="container">
-							<form action="/action_page.php">
-								<div class="row">
-									<div class="col-25">
-										<label for="fname">First Name</label>
-									</div>
-									<div class="col-75">
-									<input type="text" id="fname" name="firstname" placeholder="Your name..">
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-25">
-									<label for="lname">Last Name</label>
-									</div>
-									<div class="col-75">
-									<input type="text" id="lname" name="lastname" placeholder="Your last name..">
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-25">
-									<label for="country">Country</label>
-									</div>
-									<div class="col-75">
-									<select id="country" name="country">
-										<option value="australia">Australia</option>
-										<option value="canada">Canada</option>
-										<option value="usa">USA</option>
-									</select>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-25">
-									<label for="subject">Subject</label>
-									</div>
-									<div class="col-75">
-									<textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-									</div>
-								</div>
-								<div class="row">
-									<input type="submit" value="Submit">
-								</div>
-							</form>
-						</div>			
 				</div>
+				<div class="container">
+					<form action="/action_page.php">
+						<div class="row">
+							<div class="col-25">
+								<label for="fname">First Name</label>
+							</div>
+							<div class="col-75">
+							<input type="text" id="fname" name="firstname" placeholder="Your name..">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-25">
+								<label for="lname">Last Name</label>
+							</div>
+							<div class="col-75">
+								<input type="text" id="lname" name="lastname" placeholder="Your last name..">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-25">
+								<label for="country">Country</label>
+							</div>
+							<div class="col-75">
+							<select id="country" name="country">
+								<option value="australia">Australia</option>
+								<option value="canada">Canada</option>
+								<option value="usa">USA</option>
+							</select>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-25">
+								<label for="subject">Subject</label>
+							</div>
+							<div class="col-75">
+								<textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+							</div>
+						</div>
+						<div class="row">
+							<input type="submit" value="Submit">
+						</div>
+					</form>
+				</div>			
 			</div>
 		</div>
 	</body>
