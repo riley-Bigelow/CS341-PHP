21522a35898a29dfae36b48547f0fb17c5b8a3cb
Riley <bigelow.riley.j@gmail.com>
2020-10-23 15:15:52 -0600

    attempting fix

--- web/Projects/project1/action_add.php
+++ web/Projects/project1/action_add.php
@@ -57,8 +57,8 @@ try
 	{
 		// Again, first prepare the statement
 		
-		$statement = $db->prepare('INSERT INTO  ingredients (recipeId,ingredientName,amount,measurement,createdAt,createdBy) VALUES(:recipeId,:ingredientName,:amount,:measurement,:createdAt,:createdBy)');
-
+		$query = 'INSERT INTO  ingredients (recipeId,ingredientName,amount,measurement,createdAt,createdBy) VALUES(:recipeId,:ingredientName,:amount,:measurement,:createdAt,:createdBy)';
+		$statement = $db->prepare($query);
 		// Then, bind the values
 		$statement->bindValue(':recipeId', $recipeId);
 		$statement->bindValue(':ingredientName',  $ingredients[$x]);
@@ -70,8 +70,8 @@ try
 		$statement->execute();
 	}
 
-	$statement = $db->prepare('INSERT INTO  instructions (recipeId,instructions,createdAt,createdBy) VALUES(:recipeId,:instructions,:createdAt,:createdBy)');
-
+	$query = 'INSERT INTO  instructions (recipeId,instructions,createdAt,createdBy) VALUES(:recipeId,:instructions,:createdAt,:createdBy)';
+	$statement = $db->prepare($query);
 	// Then, bind the values
 	$statement->bindValue(':recipeId', $recipeId);
 	$statement->bindValue(':instructions',  $instructions);
