294978bbb5f9020461a0df40b91a130dfe418fcf
Riley <bigelow.riley.j@gmail.com>
2020-10-22 21:14:24 -0600

    updates

--- web/Projects/project1/search.php
+++ web/Projects/project1/search.php
@@ -42,7 +42,7 @@
 						<a href="cookBook.php"  id="cookbook"  class="button"><img src="cookbook.png" style="width:42px;height:42px;"><br>CookBook</a>
 						<a href="mealPlan.php" id ="mealplan"  class="button"><img src="mealplan.png" style="width:42px;height:42px;"><br>Meal Planner</a>
 						<a href="groceryList.php" id="glist"     class="button"><img src="grocerylist.png" style="width:42px;height:42px;"><br>Grocery List</a>
-						<!--<a href="addRecipe.php" class="button" id='add'><img src="addrecipe.png" style="width:42px;height:42px;"><br>Add Recipe</a>-->
+						<a href="addRecipe.php" class="button" id='add'><img src="addrecipe.png" style="width:42px;height:42px;"><br>Add Recipe</a>
 						<div class="search-container">
 							<form action="search.php" method="POST">
 								 <input type="text" id='search' placeholder="Search for a recipe.." name="search" required>
